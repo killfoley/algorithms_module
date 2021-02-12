e730cf734fce00a749857cc1092a649a1fddfb1e
Killian Foley <killfoley87@gmail.com>
2021-02-12 18:51:53 +0000

    largest_elem added

---
+++ largest_elem.py
@@ -0,0 +1,12 @@
+arr = [20, 300, 16, 655, 8, 30, 20, 1]
+
+def _biggest_(arr):
+    biggest = arr[0]
+    for n in range(1,len(arr)):
+        if arr[n] > biggest:
+            biggest = arr[n]
+    return biggest
+
+largest_val = _biggest_(arr)
+
+print(f'The largest element in array: arr is {largest_val}')
